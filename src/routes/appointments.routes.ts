1caa537f837ea1842f75cf6e763b2c0e78c0d148
Anderson Ramos <anderson.ramos@zoom.com.br>
2020-04-20 18:02:47 -0300

    feat: list appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,21 +1,32 @@
-import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { Router, request } from 'express';
+import { startOfHour, parseISO} from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments = [];
+// Soc: Separation of Concerns (Separação de preocupações)
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+})
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+  const parsedDate = startOfHour(parseISO(date));
+ 
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate)
 
-  appointments.push(appointment);
+  if (findAppointmentInSameDate){
+    return response.status(400).json({ message: 'This appointment is already booked' })
+  }
 
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+  
   return response.json(appointment);
 });
 
